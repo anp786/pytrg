989437d752a601b7ba81cc012e542b88781b7a3a
anp <=anoopisang@gmail.com>
2018-06-26 17:28:50 +0530

    all files added

---
+++ newLine.py
@@ -0,0 +1,18 @@
+def newLine():
+    print()
+    return 0
+print("First line")
+
+#newLine()
+print("second line")
+def threelines():
+    newLine()
+    newLine()
+    newLine()
+print("first line")
+threelines()
+print("second line")
+print(newLine()+7)
+    
+
+
